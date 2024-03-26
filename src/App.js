be9badbd2d29752ec4569e298af018e9a55b981c
Sherzodjon <124202641+tursunov4@users.noreply.github.com>
2024-03-26 21:00:14 +0500

    dfasd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './Assets/main.css'
+import Authentication from './auth/Authentication';
+import UnAuthentication from './auth/UnAuthentication';
+let token = true
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  if(token){
+    return <Authentication/>
+  }
+  else{
+    return <UnAuthentication/>
+  }
 }
 
 export default App;
---
+++ src/Pages/Regiter1/Register.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react'
+import Video from '../../Assets/Images/png/video.png'
+import http from '../../axios'
+const Register = () => {
+  const [data , setData] = useState([])
+  const [videid , setVideoid] = useState("")
+  const [modal ,setModal] = useState(false)
+  const [videos , setVideos] = useState([
+
+  ])
+
+   useEffect(()=>{
+    getCameraList()
+   } , [])
+  const getCameraList = ()=>{
+    http.get(process.env.REACT_APP_API + "/camera/list/" , ).then((res)=>{
+      console.log(res.data)
+      setData(res.data.results)
+    }).catch((err)=>{
+      console.log(err)
+      
+    })
+  }
+ 
+  const handleVideo =(id)=>{      
+      setVideos([...videos,id])
+    console.log(videos)
+  }
+  const handleVideo2 =(id)=>{
+    const newNumbers = [...videos]; // Arrayni ko'chirish (copy) qilamiz
+    const deleted = newNumbers.splice(videos.indexOf(id), 1); // Index bo'yicha elementni o'chiramiz
+    setVideos(newNumbers); // Yangi arrayni statega saqlaymiz
+  }
+
+  return (
+    <div className="outlet">
+
+      {
+        data?.map((item , index)=>(
+          <div key={index} className="outlet__kameras">
+          <h3>{item.title}</h3>
+           <div className="outlet__kameras-img">  
+           {
+            videos.includes(item.id) ?      <img onClick={()=>handleVideo2(item.id)} width={942} height={494} src={`http://64.226.102.92:8001/camera/video_feed/${item.id}/`} alt=""  /> :      <img onClick={()=>handleVideo(item.id)} width={942} height={494} src={Video} alt=""  /> 
+           }
+         </div>
+         </div>
+        ))
+      }
+    
+  
+   
+  
+  </div>
+  )
+}
+
+export default Register
